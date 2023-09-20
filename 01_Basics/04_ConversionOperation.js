a7a6aa9cf4dd23e60802f634ed535d7d676775ce
aditya2mathur <adiasfreelancer@gmail.com>
2023-09-21 00:01:18 +0530

    conversion and compression

---
+++ 01_Basics/04_ConversionOperation.js
@@ -0,0 +1,29 @@
+let mark = "33abc";
+let numName = "aditya";
+
+// console.log(Number(mark) + 33); 
+// console.log(typeof Number(mark));
+// console.log(Number(mark));
+
+// console.log(Number(numName));
+
+/* 
+"33" => 33
+33abc => NaN
+true => 1,  false => 0
+
+*/
+
+
+
+// let isLogedIn = "adi"; => true
+// let isLogedIn = "";  => false
+let isLogedIn = 1;  // => true
+let islogedOut = 0  // => false
+
+// console.log(Boolean(isLogedIn));
+// console.log(Boolean(islogedOut));
+
+
+let num = 33;
+console.log(typeof String(num))
---
+++ 01_Basics/07_string.js
@@ -0,0 +1,20 @@
+let firstName = "Aditya";
+const lastName = "mathur";
+
+// console.log("my fisrt name is " + firstName + "and last name is " + lastName); // don't use this
+
+// console.log(`my fisrt name is ${firstName} and last name is ${lastName}`);
+
+const icons = '🤔'
+console.log(icons.codePointAt());
+const newName = new String ("Code_Assaulter_Instagram_page        ");
+
+// 
+
+console.log(newName.__proto__);
+console.log(newName[0]);
+console.log(newName.length);
+console.log(newName.toLocaleUpperCase());
+console.log(newName.replaceAll('_' ,'-'));
+console.log(newName.trim());
+
